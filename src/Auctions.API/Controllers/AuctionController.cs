5324eeb485e44470826a9f788851f5f7adab2b9b
Kleber Gomes <kleber.gsilva@outlook.com>
2024-02-06 21:41:05 -0300

    Auctions - Criação do projeto
    Foi feito a criação e configuração dos primeiros arquivos do projeto de leilão em dotNet

---
+++ src/Auctions.API/Controllers/AuctionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auctions.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrent() 
+        {
+            return Ok("kleber");
+        }
+    }
+}
